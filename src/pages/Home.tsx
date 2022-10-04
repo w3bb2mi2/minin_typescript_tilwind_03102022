60c867a038992e46a37b4eb1bde5f2eacae98c3e
Eduard <w3bb2mi2@gmail.com>
2022-10-04 20:18:53 +0300

    json2typescript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,8 @@
-export const Home = ()=>{
-    return(
+import { useSearchUsersQuery } from "../store/github/github.api"
+
+export const Home = () => {
+    const {isLoading, isError, data } = useSearchUsersQuery("eduard")
+    return (
         <div>Home</div>
     )
 }
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { IUser, ServerResponse } from "../../models/models"
 
 export const githubApi = createApi({
     reducerPath: "github/api",
@@ -6,13 +7,15 @@ export const githubApi = createApi({
         baseUrl: "https://api.github.com/"
     }),
     endpoints: build => ({
-        searchUsers: build.query<any, string>({
+        searchUsers: build.query<IUser[], string>({
             query: (search: string) => ({
                 url: `search/users`,
                 params: {
-                    q: search
+                    q: search,
+                    per_page: 10
                 }
-            })
+            }),
+            transformResponse: (res: ServerResponse<IUser>)=>{return res.items}
         })
     })
 })

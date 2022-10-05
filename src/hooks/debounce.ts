0fb91b68ee3c35fdac5e3327f3cc7334d785587a
Eduard <w3bb2mi2@gmail.com>
2022-10-05 08:50:04 +0300

    сделан выпадающий список

--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -1,10 +1,11 @@
 import { debug } from "console";
 import { useEffect, useState } from "react";
 
-export function useDebounce(value:string, delay=3000):string{
+export function useDebounce(value:string, delay=300):string{
     
     const [debounce, setDebounce] = useState(value)
     useEffect(()=>{
+        
         const handler = setTimeout(()=>{setDebounce(value);console.log({debounce})}, delay)
         return ()=>{clearTimeout(handler); console.log("clearTimeout")}
     },[value, delay])
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,27 +4,36 @@ import { useSearchUsersQuery } from "../store/github/github.api"
 
 export const Home = () => {
     const [search, setSearch] = useState("");
-    const {isLoading, isError, data } = useSearchUsersQuery("eduard")
-    
+    const [dropdown, setDropdown] = useState(false)
     const debounced = useDebounce(search)
+    const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
+        skip: debounced.length < 3
+    })
 
-    useEffect(()=>{
-        console.log(search)
-    },[debounced])
+
+    useEffect(() => {
+        setDropdown(debounced.length>3 && data?.length! > 0)
+    }, [debounced])
     return (
         <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
             {isError && <p className="text-center text-red-600">Something went wrong...</p>}
             <div className="relative w-[560px]">
-                <input 
+                <input
                     type="text"
                     className="border py-2 px-4 w-full h-[42px] mb-2"
                     placeholder="Search for Github username..."
                     value={search}
-                    onChange={e=>setSearch(e.target.value)}
+                    onChange={e => setSearch(e.target.value)}
                 />
-                <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, vitae ullam, architecto natus quibusdam eaque tempore ad quaerat hic repudiandae doloremque! Aperiam, non odio. Nam quae quia magni commodi libero!
-                </div>
+                {dropdown && <ul className="overflow-hidden list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+                    {isLoading && <p className="text-center">Loading...</p>}
+                    {data?.map(user => (
+                        <li
+                            key={user.id}
+                            className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+                        >{user.login}</li>
+                    ))}
+                </ul>}
             </div>
         </div>
     )
